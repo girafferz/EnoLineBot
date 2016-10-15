37e61a0849001ff6aa2bead8e4130cc373f254af
Hiroto Kitamura <kirikonknow@gmail.com>
2016-10-15 13:22:34 +0900

    webhookの処理を別ファイル化

---
+++ toki3_nyan/webhook.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"github.com/line/line-bot-sdk-go/linebot"
+	"time"
+	"google.golang.org/appengine"
+	"net/http"
+	"google.golang.org/appengine/taskqueue"
+	"encoding/json"
+	"encoding/base64"
+	"net/url"
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/urlfetch"
+	"github.com/line/line-bot-sdk-go/linebot/httphandler"
+)
+
+var botHandler *httphandler.WebhookHandler
+
+func SetBotHandler(aBotHandler *httphandler.WebhookHandler) {
+	botHandler = aBotHandler
+}
+
+// Webhook を受け取って TaskQueueに詰める関数
+func HandleCallback(evs []*linebot.Event, r *http.Request) {
+	c := appengine.NewContext(r)
+	ts := make([]*taskqueue.Task, len(evs))
+	for i, e := range evs {
+		j, err := json.Marshal(e)
+		if err != nil {
+			log.Errorf(c, "json.Marshal: %v", err)
+			return
+		}
+		data := base64.StdEncoding.EncodeToString(j)
+		t := taskqueue.NewPOSTTask("/task", url.Values{"data": {data}})
+		ts[i] = t
+	}
+	taskqueue.AddMulti(c, ts, "")
+}
+
+// 受け取ったメッセージを実際に処理する
+func HandleTask(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	data := r.FormValue("data")
+	if data == "" {
+		log.Errorf(c, "No data")
+		return
+	}
+
+	j, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		log.Errorf(c, "base64 DecodeString: %v", err)
+		return
+	}
+
+	event := new(linebot.Event)
+	err = json.Unmarshal(j, event)
+	if err != nil {
+		log.Errorf(c, "json.Unmarshal: %v", err)
+		return
+	}
+
+	bot, err := botHandler.NewClient(linebot.WithHTTPClient(urlfetch.Client(c)))
+	if err != nil {
+		log.Errorf(c, "newLINEBot: %v", err)
+		return
+	}
+
+	switch event.Type {
+	case "postback":
+	case "message":
+
+	}
+
+	log.Infof(c, "EventType: %s\nMessage: %#v", event.Type, event.Message)
+
+	m := getResponseMessage(event)
+	if _, err = bot.ReplyMessage(event.ReplyToken, m).WithContext(c).Do(); err != nil {
+		log.Errorf(c, "ReplayMessage: %v", err)
+		return
+	}
+
+	w.WriteHeader(200)
+}
+
+func getResponseMessage(event *linebot.Event) linebot.Message {
+	switch event.Type {
+	case "postback":
+		return onReceivePostBack(event.Postback.Data)
+	case "message":
+		switch message := event.Message.(type) {
+		case *linebot.TextMessage:
+			return onReceiveTextMessage(message.Text)
+		case *linebot.ImageMessage:
+			return linebot.NewTextMessage("画像だにゃん")
+		case *linebot.StickerMessage:
+			return linebot.NewTextMessage("スタンプだにゃん")
+		default:
+			return linebot.NewTextMessage("良くわかんないにゃん")
+		}
+	}
+	return linebot.NewTextMessage("良くわかんないにゃん")
+}
+
+func onReceiveTextMessage(text string) linebot.Message {
+	switch text {
+	case "はらへ":
+		template := linebot.NewConfirmTemplate(
+			"なにがたべたいにゃん",
+			linebot.NewPostbackTemplateAction("らーめん", "ramen", ""),
+			linebot.NewPostbackTemplateAction("びーる", "beer", ""))
+		return linebot.NewTemplateMessage("らーめん種類選択", template)
+	case "今の時間をおしえて！":
+		t := time.Now().In(time.FixedZone("Asia/Tokyo", 9 * 60 * 60))
+		return linebot.NewTextMessage(t.Format(time.Kitchen))
+	default:
+		return linebot.NewTextMessage("理解できない言葉だにゃん＞＜")
+	}
+}
+
+func onReceivePostBack(data string) linebot.Message {
+	switch data {
+	case "ramen":
+		return linebot.NewTextMessage("らーめん探すにゃん")
+	case "beer":
+		return linebot.NewTextMessage("ビール探すにゃん")
+	default:
+		return linebot.NewTextMessage("良くわかんないにゃん")
+	}
+}
